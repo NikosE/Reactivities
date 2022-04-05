3fd6a0ed6fd599c47615514209ae1e5dc17e68f2
Nikos <neuthimiou83@gmail.com>
2022-04-05 21:46:16 +0300

    End of section 9

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import 'semantic-ui-css/semantic.min.css';
+import "react-calendar/dist/Calendar.css";
 import "../src/app/layout/styles.css";
 import { store, StoreContext } from "./app/stores/store";
 import App from "./app/layout/App";
